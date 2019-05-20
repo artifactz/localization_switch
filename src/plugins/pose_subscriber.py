61c303a241852102f967da3e37b2e9110ae5abc3
Jan Brehmer <jan.brehmer@dai-labor.de>
2019-05-20 17:49:51 +0200

    :sparkles: extended PoseSubscriber to handle stamped, plain and covariance poses

--- src/plugins/pose_subscriber.py
+++ src/plugins/pose_subscriber.py
@@ -1,5 +1,5 @@
 import rospy
-from geometry_msgs.msg import PoseWithCovarianceStamped
+from geometry_msgs.msg import Pose, PoseStamped, PoseWithCovariance, PoseWithCovarianceStamped
 
 from plugin_base import AbstractLocalizationSubscriber, get_relative_transform
 import plotting
@@ -7,12 +7,19 @@ import plotting
 
 class PoseSubscriber(AbstractLocalizationSubscriber):
     '''subscribes to a PoseWithCovarianceStamped topic and hands in delta transforms to its callback'''
-    def __init__(self, pose_topic='/pose', timeout=1., **kwargs):  # TODO: implement timeout
+    def __init__(self, pose_topic='/pose', is_with_covariance=False, is_stamped=False, timeout=1., **kwargs):  # TODO: implement timeout
         super(PoseSubscriber, self).__init__(**kwargs)
         self.enabled = False
         # params
         self.pose_topic = pose_topic
-        self.sub_pose = rospy.Subscriber(self.pose_topic, PoseWithCovarianceStamped, self.pose_callback)
+        # pose type
+        self.is_with_covariance = is_with_covariance
+        self.is_stamped = is_stamped
+        if is_with_covariance:
+            pose_type = PoseWithCovarianceStamped if is_stamped else PoseWithCovariance
+        else:
+            pose_type = PoseStamped if is_stamped else Pose
+        self.sub_pose = rospy.Subscriber(self.pose_topic, pose_type, self.pose_callback)
 
         self.last_pose = None
 
@@ -21,17 +28,20 @@ class PoseSubscriber(AbstractLocalizationSubscriber):
 
     def pose_callback(self, msg):
         '''handles arrival of pose messages'''
+        # strip potential header and cov
+        pose = msg.pose if self.is_stamped else msg
+        pose = pose.pose if self.is_with_covariance else pose
         if self.last_pose is not None:
             # get difference
             delta_transform = get_relative_transform(
                 self.last_pose.position, self.last_pose.orientation,
-                msg.pose.pose.position, msg.pose.pose.orientation)
+                pose.position, pose.orientation)
 
             # hand in the update
             self.callback(delta_transform)
             if self.do_plot:
                 plotting.add_pose(self, delta_transform)
 
-        self.last_pose = msg.pose.pose
+        self.last_pose = pose
 
         self.enabled = True
